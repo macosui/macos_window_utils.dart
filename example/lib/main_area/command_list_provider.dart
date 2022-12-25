ed348ebd9e9b68663b84e5f02502ebdf006a5cbc
Adrian Samoticha <adrian@samoticha.de>
2022-12-25 23:52:38 +0100

    add more commands to command list in example

--- example/lib/main_area/command_list_provider.dart
+++ example/lib/main_area/command_list_provider.dart
@@ -28,6 +28,60 @@ class CommandListProvider {
         name: 'setDocumentUnedited()',
         function: () => WindowManipulator.setDocumentUnedited(),
       ),
+      Command(
+        name: 'setRepresentedFilename(\'some file.txt\')',
+        function: () =>
+            WindowManipulator.setRepresentedFilename('some file.txt'),
+      ),
+      Command(
+        name: 'setRepresentedFilename(\'\')',
+        function: () => WindowManipulator.setRepresentedFilename(''),
+      ),
+      Command(
+        name: 'setRepresentedUrl(\'http://some_url.com/\')',
+        function: () =>
+            WindowManipulator.setRepresentedUrl('http://some_url.com/'),
+      ),
+      Command(
+        name: 'setRepresentedUrl(\'\')',
+        function: () => WindowManipulator.setRepresentedUrl(''),
+      ),
+      Command(
+        name: 'hideTitle()',
+        function: () => WindowManipulator.hideTitle(),
+      ),
+      Command(
+        name: 'showTitle()',
+        function: () => WindowManipulator.showTitle(),
+      ),
+      Command(
+        name: 'makeTitlebarTransparent()',
+        function: () => WindowManipulator.makeTitlebarTransparent(),
+      ),
+      Command(
+        name: 'makeTitlebarOpaque()',
+        function: () => WindowManipulator.makeTitlebarOpaque(),
+      ),
+      Command(
+        name: 'enableFullSizeContentView()',
+        description: 'Enables the window\'s full-size content view.\n\n'
+            'This expands the area that Flutter can draw to to fill the entire'
+            ' window. It is recommended to enable the full-size content view '
+            'when making the titlebar transparent.',
+        function: () => WindowManipulator.enableFullSizeContentView(),
+      ),
+      Command(
+        name: 'disableFullSizeContentView()',
+        function: () => WindowManipulator.disableFullSizeContentView(),
+      ),
+      Command(
+        name: 'zoomWindow()',
+        function: () => WindowManipulator.zoomWindow(),
+      ),
+      Command(
+        name: 'unzoomWindow()',
+        function: () => WindowManipulator.unzoomWindow(),
+      ),
     ];
   }
 }
