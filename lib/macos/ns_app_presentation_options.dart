e485e6847c4a4dd0f839d47b4b9f1982e46a6684
Adrian Samoticha <adrian@samoticha.de>
2023-02-27 15:06:19 +0100

    document ns app presentation options

--- lib/macos/ns_app_presentation_options.dart
+++ lib/macos/ns_app_presentation_options.dart
@@ -3,38 +3,72 @@ import 'package:macos_window_utils/window_manipulator/window_manipulator.dart';
 
 export 'package:macos_window_utils/macos/ns_app_presentation_option.dart';
 
-/// TODO: document this
+/// Constants that control the presentation of the app, typically for fullscreen
+/// apps such as games or kiosks.
+///
+/// Usage example:
+/// ```dart
+/// NSAppPresentationOptions.from({
+///   NSAppPresentationOption.autoHideToolbar,
+///   NSAppPresentationOption.fullScreen,
+///   NSAppPresentationOption.autoHideMenuBar,
+///   NSAppPresentationOption.hideDock,
+/// }).apply();
+/// ```
 class NSAppPresentationOptions {
+  /// The options contained within this object.
   final Set<NSAppPresentationOption> options;
 
+  /// Creates a [NSAppPresentationOptions] instance from a set.
   const NSAppPresentationOptions._fromSet(this.options);
 
+  /// An empty [NSAppPresentationOptions] instance.
   static NSAppPresentationOptions empty =
       NSAppPresentationOptions._fromSet(const {});
 
+  /// Creates a [NSAppPresentationOptions] instance from any collectible.
   factory NSAppPresentationOptions.from(
       Iterable<NSAppPresentationOption> iterable) {
     return NSAppPresentationOptions._fromSet(Set.from(iterable));
   }
 
+  /// Returns a [NSAppPresentationOptions] with [option] inserted.
   NSAppPresentationOptions insert(NSAppPresentationOption option) {
     final newSet = {...options, option};
     return NSAppPresentationOptions.from(newSet);
   }
 
+  /// Returns a [NSAppPresentationOptions] with [option] removed.
   NSAppPresentationOptions remove(NSAppPresentationOption option) {
     final newSet = options.where((element) => element != option);
     return NSAppPresentationOptions.from(newSet);
   }
 
+  /// Returns whether this instance contains [option].
   bool contains(NSAppPresentationOption option) {
     return options.contains(option);
   }
 
+  /// Returns whether this instance contains all [options].
   bool containsAll(Iterable<NSAppPresentationOption> options) {
     return this.options.containsAll(options);
   }
 
+  /// Throws an assertion error if this instance violates any of the following
+  /// restrictions:
+  ///
+  /// + `autoHideDock` and `hideDock` are mutually exclusive: You may specify
+  ///   one or the other, but not both.
+  /// + `autoHideMenuBar` and `hideMenuBar` are mutually exclusive: You may
+  ///   specify one or the other, but not both.
+  /// + If you specify `hideMenuBar`, it must be accompanied by `hideDock`.
+  /// + If you specify `autoHideMenuBar`, it must be accompanied by either
+  ///   `hideDock` or `autoHideDock`.
+  /// + If you specify any of `disableProcessSwitching`, `disableForceQuit`,
+  ///   `disableSessionTermination`, or `disableMenuBarTransparency`, it must
+  ///   be accompanied by either `hideDock` or `autoHideDock`.
+  /// + `autoHideToolbar` may be used only when both `fullScreen` and
+  ///   `autoHideMenuBar` are also set.
   void assertRestrictions() {
     assert(
         !options.contains(NSAppPresentationOption.autoHideDock) ||
@@ -81,6 +115,22 @@ class NSAppPresentationOptions {
         'autoHideMenuBar are also set.');
   }
 
+  /// Applies these [NSAppPresentationOptions] to the window.
+  ///
+  /// Throws an assertion error if any of the following restrictions is
+  /// violated:
+  /// + `autoHideDock` and `hideDock` are mutually exclusive: You may specify
+  ///   one or the other, but not both.
+  /// + `autoHideMenuBar` and `hideMenuBar` are mutually exclusive: You may
+  ///   specify one or the other, but not both.
+  /// + If you specify `hideMenuBar`, it must be accompanied by `hideDock`.
+  /// + If you specify `autoHideMenuBar`, it must be accompanied by either
+  ///   `hideDock` or `autoHideDock`.
+  /// + If you specify any of `disableProcessSwitching`, `disableForceQuit`,
+  ///   `disableSessionTermination`, or `disableMenuBarTransparency`, it must
+  ///   be accompanied by either `hideDock` or `autoHideDock`.
+  /// + `autoHideToolbar` may be used only when both `fullScreen` and
+  ///   `autoHideMenuBar` are also set.
   void apply() {
     assertRestrictions();
 
