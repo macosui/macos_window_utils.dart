0fc6e4f4b930a705863723a138880b0395ee57aa
Adrian Samoticha <adrian@samoticha.de>
2023-02-28 15:14:08 +0100

    document ns window delegate event handler

--- example/lib/main_area/ns_window_delegate_demo/ns_window_delegate_event_handler.dart
+++ example/lib/main_area/ns_window_delegate_demo/ns_window_delegate_event_handler.dart
@@ -3,17 +3,28 @@ import 'dart:collection';
 
 import 'package:example/main_area/ns_window_delegate_demo/ns_window_delegate_event.dart';
 
-/// TODO: document this
+/// A class that handles [NSWindowDelegateEvent]s.
 class NSWindowDelegateEventHandler {
+  /// A list of captured events.
   final _events = <NSWindowDelegateEvent>[];
+
+  /// A stream controller for a stream that streams changes to [events].
   final _onChangedStreamController =
       StreamController<List<NSWindowDelegateEvent>>.broadcast();
 
+  /// An [UnmodifiableListView] of the [NSWindowDelegateEvent]s captured by this
+  /// [NSWindowDelegateEventHandler].
   List<NSWindowDelegateEvent> get events => UnmodifiableListView(_events);
 
+  /// A stream that streams changes to [events].
   Stream<List<NSWindowDelegateEvent>> get onChangedStream =>
       _onChangedStreamController.stream;
 
+  /// Adds a captured event to [events].
+  ///
+  /// If the newly added event matches the previously added event, its number of
+  /// occurrences is incremented, rather than inserting a new
+  /// [NSWindowDelegateEvent].
   void addEvent(NSWindowDelegateEvent event) {
     if (_events.isEmpty) {
       _events.add(event);
