62f417b26f3b75872437e5cfd0d4b4dc528eb889
Adrian Samoticha <86920182+Adrian-Samoticha@users.noreply.github.com>
2025-03-30 11:53:55 +0200

    start `MainFlutterWindowManipulator` when `reset` is called and `MainFlutterWindow` is nil

--- macos/Classes/MainFlutterWindowManipulator.swift
+++ macos/Classes/MainFlutterWindowManipulator.swift
@@ -48,6 +48,11 @@ public class MainFlutterWindowManipulator {
     }
     
     public static func reset() {
+        if (self.mainFlutterWindow == nil) {
+            start(mainFlutterWindow: nil)
+            return;
+        }
+        
         passthroughViewHandler.start(mainFlutterWindow: self.mainFlutterWindow!)
     }
     
